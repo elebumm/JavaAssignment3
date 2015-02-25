adf5810296896d50e98d675fab170a3346be29d5
Dylan <dylan@dylanweb.ca>
2015-02-25 01:36:45 -0500

    Initial Commit. Created New Java Web Project. Created new RESTful Web Service from Database Connected service to rbp_cpro database at gator3119.hostgator.com, for user: rpb_cprousers and password: Cpro_1. Named starting package 'entities'.
    NetBeans auto-generated the following Source Packages and files:
    entities - containing Product.java
    entities.service - containing AbstractFacade.java, ApplicationConfig.java, and ProductFacadeREST.java
    
    Created new package: 'credentials'.
    Added Credentials.java file, which includes getConnection() method, using credentials given in DB connection.

---
+++ src/java/credentials/Credentials.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package credentials;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Dylan Huculak - c0630163
+ */
+public class Credentials {
+    
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("gator3119.hostgator.com");
+            String db = "rbp_cpro";
+            String user = "rbp_cprouser";
+            String pass = "Cpro_1";
+            conn = DriverManager.getConnection(db, user, pass);
+        }
+        catch (ClassNotFoundException | SQLException ex){
+            Logger.getLogger(Credentials.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+    
+}
