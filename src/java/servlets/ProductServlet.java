da1d72c15c4d62d8d548bff089d096072e0b8064
Lewis Menelaws <lmenelaws112@gmail.com>
2015-02-28 17:47:25 -0500

    Added all function for servlet

--- src/java/servlets/ProductServlet.java
+++ src/java/servlets/ProductServlet.java
@@ -5,8 +5,18 @@
  */
 package servlets;
 
+import credentials.Credentials;
+import static credentials.Credentials.getConnection;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,24 +37,98 @@ public class ProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+             response.setHeader("Content-Type", "text/plain-text");
+             try (PrintWriter out = response.getWriter()) {
+                 if (!request.getParameterNames().hasMoreElements()){
+                     out.println(getResults("SELECT * FROM product"));
+                 }
+                 else {
+                     String id = request.getParameter("id");
+                     out.println(getResults("SELECT * FROM product WHERE ProductID = ?", id));
+                     
+                 }
+             } catch (IOException ex){
+                 Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+             }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        String result;
+        Set<String> keySet = request.getParameterMap().keySet();
+        try (PrintWriter out = response.getWriter()) {
+            if (keySet.contains("name") && keySet.contains("description") && keySet.contains("quantity")) {
+                String name = request.getParameter("name");
+                String description = request.getParameter("description");
+                String quantity = request.getParameter("quantity");
+                result = ("INSERT INTO product (Name, Description, Quantity) VALUES (?,?,?)", name, description, quantity);
+                
+                if (result.equalsIgnoreCase("BAD")){
+                    response.setStatus(500);
+                } else {
+                    out.println(result);
+                } 
+            
+            }
+            else {
+                out.println("Error: Not enough data to input");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
+
+    
+    
     //MANUALLY GENERATED doPut AND doDelete METHODS---------------
     String doPut(String id, String name, String desc, int quan) {
         
         return url;
     }
     
-    String doDelete(String id) {
-        
-        return result;
+    private String getResults(String query, String... params){
+        StringBuilder sb = new StringBuilder();
+        try (Connection conn = Credentials.getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            sb.append("[ ");
+            while (rs.next()) {
+                sb.append(String.format("{ \"productId\" : %s, \"name\" : %s, \"description\" : %s, \"quantity\" : %s },\n", rs.getInt("ProductID"), rs.getString("Name"), rs.getString("Description"), rs.getInt("Quantity")));
+            }
+            sb.setLength(Math.max(sb.length() - 2, 0));
+            sb.append("]");
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sb.toString();
     }
-
+    
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Set<String> keySet = request.getParameterMap().keySet();
+        try (PrintWriter out = response.getWriter()) {
+            Connection conn = getConnection();
+            if (keySet.contains("productID")) {
+                PreparedStatement pstmt = conn.prepareStatement("DELETE FROM product WHERE productID = " + request.getParameter("productID"));
+                try {
+                    pstmt.executeUpdate();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    out.println("Error deleting entry");
+                    response.setStatus(500);
+                }
+            } else {
+                out.println("No data to delete");
+                response.setStatus(500);
+            }
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+            
 }
