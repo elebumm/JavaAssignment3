8cf184bd590fad23e33bbf3e8eccdc8eaec9201f
Dylan <dylan@dylanweb.ca>
2015-03-05 17:55:03 -0500

    Changed indendation of doGet method

--- src/java/services/Products.java
+++ src/java/services/Products.java
@@ -51,7 +51,8 @@ public class Products {
     @Produces("application/json")
     public Response doGet(@PathParam("productId") int id) {
         return Response.ok(getResults("SELECT * FROM products WHERE productId = " 
-                + String.valueOf(id)), MediaType.APPLICATION_JSON).build();   
+                + String.valueOf(id)),
+                    MediaType.APPLICATION_JSON).build();   
     }
 
     /** 
