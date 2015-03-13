3c8fe6bf2cb32ed8a06fc86f7e9dd08659f91db1
Lewis Menelaws <lmenelaws112@gmail.com>
2015-03-13 10:19:23 -0400

    Fixed some typos that wouldn't let the code run.

--- src/java/servlets/ProductServlet.java
+++ src/java/servlets/ProductServlet.java
@@ -24,6 +24,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 /**
@@ -75,7 +76,7 @@ public class ProductServlet extends HttpServlet {
     }
     
     @PUT
-    @PATH("{id}")
+    @Path("{id}")
     @Consumes("application/json")
     protected void doPut(HttpServletRequest request, HttpServletResponse response) 
             throws IOException, ServletException {
